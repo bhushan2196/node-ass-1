235206dfe67535905d47a9da81035a1770e12ae7
Bhushan Mahajan <140087711+bhushan2196@users.noreply.github.com>
2024-07-04 16:05:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const crypto = require('crypto');
+
+
 let value=process.argv.slice(2)
 
 let opration = value[0]
@@ -65,6 +68,18 @@ function tan(number) {
     return Math.tan(number);
 }
 
+// random
+function random1(length){
+    if(!length){
+        console.log(`provide length`);
+        return
+    }
+    const readomBytes= crypto.randomBytes(Number(length))
+    return readomBytes.toString('binary')
+}
+// console.log(random1(10));
+
+
 if(opration=='add' || opration=='sub' || opration=='mult' || opration=='div'  ){
     if(opration=="add" && numbers.length>1){
         console.log("result:",add(numbers));
@@ -83,7 +98,7 @@ if(opration=='add' || opration=='sub' || opration=='mult' || opration=='div'  ){
       }
 }
 
-if(opration=="sin" || opration =="tan" ||opration == "cos"){
+if(opration=="sin" || opration =="tan" ||opration == "cos" || opration=="random"){
     if(opration=="sin" && numbers.length==1){
         console.log("result:",sin(numbers));
      }
@@ -93,6 +108,9 @@ if(opration=="sin" || opration =="tan" ||opration == "cos"){
     
      else if(opration=="tan" && numbers.length==1){
         console.log("result:",tan(numbers));
+     }
+     else if(opration=="random" && numbers.length==1){
+        console.log("result:",random1(numbers));
      }
      else{
         console.log(`Please provide an operation and only one number.`);
