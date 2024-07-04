0a5e1f1a411f9165c3e325f45dfc1e0c45800576
Bhushan Mahajan <140087711+bhushan2196@users.noreply.github.com>
2024-07-04 15:54:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+let value=process.argv.slice(2)
+
+let opration = value[0]
+
+let numbers=value.slice(1)
+
+if(opration==undefined ){
+    console.log(`Please provide an operation(add, sub, mult, div, sin, cos, tan, random) and at least one number.`);
+}
+
+// addition
+function add(arr){
+    let output=0
+    for(let num of arr){
+        output+=Number( num)
+    }
+    return output
+}
+
+// subtraction 
+function sub(arr){
+    let output=numbers[0]
+    for(let i=1;i<numbers.length;i++){
+        output-=Number( numbers[i])
+    }
+    return output
+}
+
+// multiply
+function mult(arr){
+    let output=1
+    for(let num of arr){
+        output*=Number( num)
+    }
+    return output
+}
+
+
+// division
+function div(arr){
+    let result=numbers[0]
+    for(let i=1;i<numbers.length;i++){
+        result/=Number( numbers[i])
+        if(Number( numbers[i])==0){
+            console.log(`Error: Division by zero`);
+            return result=`try another number`
+        }
+    }
+    return result
+}
+
+
+// sin
+function sin(number) {
+    return Math.sin(number);
+}
+
+// cos
+function cos(number) {
+    return Math.cos(number);
+}
+
+// tan
+function tan(number) {
+    return Math.tan(number);
+}
+
+if(opration=='add' || opration=='sub' || opration=='mult' || opration=='div'  ){
+    if(opration=="add" && numbers.length>1){
+        console.log("result:",add(numbers));
+     }
+     else if(opration=="sub" && numbers.length>1){
+         console.log("result:",sub(numbers));
+      }
+      else if(opration=="mult" && numbers.length>1){
+         console.log("result:",mult(numbers));
+      }
+      else if(opration=="div" && numbers.length>1){
+         console.log("result:",div(numbers));
+      }
+      else{
+         console.log(`Please provide an operation and at least one number .`);
+      }
+}
+
+if(opration=="sin" || opration =="tan" ||opration == "cos"){
+    if(opration=="sin" && numbers.length==1){
+        console.log("result:",sin(numbers));
+     }
+     else if(opration=="cos" && numbers.length==1){
+        console.log("result:",cos(numbers));
+     }
+    
+     else if(opration=="tan" && numbers.length==1){
+        console.log("result:",tan(numbers));
+     }
+     else{
+        console.log(`Please provide an operation and only one number.`);
+     }
+    
+    
+}
